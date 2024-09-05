6544ae9572cbf42fedcf6232b5eacd26cc7a23a7
Daisuke SHIGETA <shigepon7@gmail.com>
2024-09-05 14:14:25 +0900

    post thread & japanese thread

--- src/firehose.rs
+++ src/firehose.rs
@@ -69,27 +69,105 @@ pub async fn receiver_thread(
   }
 }
 
+pub async fn post_thread(
+  mut receiver: tokio::sync::mpsc::Receiver<(ComAtprotoSyncSubscribeReposCommit, Record)>,
+  post_receivers: std::sync::Arc<
+    tokio::sync::RwLock<
+      Vec<tokio::sync::mpsc::Sender<(ComAtprotoSyncSubscribeReposCommit, AppBskyFeedPost)>>,
+    >,
+  >,
+) {
+  loop {
+    let (commit, record) = match receiver.recv().await {
+      Some(p) => p,
+      None => continue,
+    };
+    let post = match record.as_app_bsky_feed_post() {
+      Some(p) => p,
+      None => continue,
+    };
+    for tx in post_receivers.read().await.iter() {
+      if let Err(e) = tx.send((commit.clone(), post.clone())).await {
+        tracing::warn!("POST_RECEIVER : send record error {e}");
+      }
+    }
+  }
+}
+
+pub async fn japanese_thread(
+  mut receiver: tokio::sync::mpsc::Receiver<(ComAtprotoSyncSubscribeReposCommit, AppBskyFeedPost)>,
+  ja_receivers: std::sync::Arc<
+    tokio::sync::RwLock<
+      Vec<tokio::sync::mpsc::Sender<(ComAtprotoSyncSubscribeReposCommit, AppBskyFeedPost)>>,
+    >,
+  >,
+) {
+  loop {
+    let (commit, post) = match receiver.recv().await {
+      Some(p) => p,
+      None => continue,
+    };
+    if post
+      .langs
+      .as_ref()
+      .map(|l| l.contains(&String::from("ja")))
+      .unwrap_or(false)
+    {
+      for tx in ja_receivers.read().await.iter() {
+        if let Err(e) = tx.send((commit.clone(), post.clone())).await {
+          tracing::warn!("JA_RECEIVER : send record error {e}");
+        }
+      }
+    }
+  }
+}
+
 pub struct Firehose {
   pub handles: indexmap::IndexMap<String, tokio::task::JoinHandle<()>>,
   pub tx: tokio::sync::mpsc::Sender<(ComAtprotoSyncSubscribeReposCommit, Record)>,
   pub rx_hd: tokio::task::JoinHandle<()>,
+  pub post_rx_hd: tokio::task::JoinHandle<()>,
+  pub ja_rx_hd: tokio::task::JoinHandle<()>,
   pub receivers: std::sync::Arc<
     tokio::sync::RwLock<
       Vec<tokio::sync::mpsc::Sender<(ComAtprotoSyncSubscribeReposCommit, Record)>>,
+    >,
+  >,
+  pub post_receivers: std::sync::Arc<
+    tokio::sync::RwLock<
+      Vec<tokio::sync::mpsc::Sender<(ComAtprotoSyncSubscribeReposCommit, AppBskyFeedPost)>>,
+    >,
+  >,
+  pub ja_receivers: std::sync::Arc<
+    tokio::sync::RwLock<
+      Vec<tokio::sync::mpsc::Sender<(ComAtprotoSyncSubscribeReposCommit, AppBskyFeedPost)>>,
     >,
   >,
 }
 
 impl Firehose {
   pub fn new(size: usize) -> Self {
+    let ja_receivers = std::sync::Arc::new(tokio::sync::RwLock::new(Vec::new()));
+    let (ja_tx, ja_rx) = tokio::sync::mpsc::channel(size);
+    let ja_rx_hd = tokio::spawn(japanese_thread(ja_rx, ja_receivers.clone()));
+
+    let post_receivers = std::sync::Arc::new(tokio::sync::RwLock::new(vec![ja_tx]));
+    let (post_tx, post_rx) = tokio::sync::mpsc::channel(size);
+    let post_rx_hd = tokio::spawn(post_thread(post_rx, post_receivers.clone()));
+
     let (tx, rx) = tokio::sync::mpsc::channel(size);
-    let receivers = std::sync::Arc::new(tokio::sync::RwLock::new(Vec::new()));
+    let receivers = std::sync::Arc::new(tokio::sync::RwLock::new(vec![post_tx]));
     let rx_hd = tokio::spawn(receiver_thread(rx, receivers.clone()));
+
     Self {
       handles: indexmap::IndexMap::new(),
       tx,
       rx_hd,
+      post_rx_hd,
+      ja_rx_hd,
       receivers,
+      post_receivers,
+      ja_receivers,
     }
   }
 
@@ -106,6 +184,24 @@ impl Firehose {
   ) -> tokio::sync::mpsc::Receiver<(ComAtprotoSyncSubscribeReposCommit, Record)> {
     let (sender, receiver) = tokio::sync::mpsc::channel(size);
     self.receivers.write().await.push(sender);
+    receiver
+  }
+
+  pub async fn add_post_receiver(
+    &mut self,
+    size: usize,
+  ) -> tokio::sync::mpsc::Receiver<(ComAtprotoSyncSubscribeReposCommit, AppBskyFeedPost)> {
+    let (sender, receiver) = tokio::sync::mpsc::channel(size);
+    self.post_receivers.write().await.push(sender);
+    receiver
+  }
+
+  pub async fn add_ja_receiver(
+    &mut self,
+    size: usize,
+  ) -> tokio::sync::mpsc::Receiver<(ComAtprotoSyncSubscribeReposCommit, AppBskyFeedPost)> {
+    let (sender, receiver) = tokio::sync::mpsc::channel(size);
+    self.ja_receivers.write().await.push(sender);
     receiver
   }
 }
